ebecce3898bda49bec07793b1fc2c2600ce61e22
IHavenofriendss <96544729+IHavenofriendss@users.noreply.github.com>
2022-01-29 20:23:21 +0530

    Update Messages.py

--- Messages.py
+++ Messages.py
@@ -1,7 +1,7 @@
-m_start = "⚡️ SELAMAT DATANG ⚡️\n\n🇮🇩 Semoga Dapat teman atau jodoh\n\n🇳🇿 I hope you can make a friend or a partner\n\n💬 untuk mencari teman obrolan gunakan button New Chat"
+m_start = "⚡️ Welcome ⚡️\n\nI hope you can make a friend or a partner\n\n💬 to find chat friends use the New Chat button‌‌"
 
 m_is_not_free_users = (
-    "❗️ Maaf Anda tidak berada dalam obrolan\n\n❗️ Sorry you are not in chat"
+    "\n\n❗️ Sorry you are not in chat"
 )
 
 m_is_connect = "✅ Anda sudah berada di dalam obrolan, silahkan kirim chat anda\n\n✅ You are already in the chat, please send your chat"
@@ -10,27 +10,27 @@ m_play_again = "Apakah Anda Ingin mengobrol dengan orang lain?\n\nDo you want to
 
 m_is_not_user_name = "❌ Maaf, bot kami hanya dapat berkomunikasi jika Anda memiliki nama pengguna\n\n❌ Sorry, our bot can only communicate if you have a username"
 
-m_good_bye = "❌ Maaf lawan bicara anda meninggalkan obrolan\n\n❌ Sorry your interlocutor left the chat"
+m_good_bye = "\n\n❌ Sorry your interlocutor left the chat"
 
-m_disconnect_user = "Maaf sambungan telah terputus!\n\nSorry, the connection was lost!"
+m_disconnect_user = "\n\nSorry, the connection was lost!"
 
-m_failed = "❌ Terjadi kesalahan!\n\n❌ Loss happened"
+m_failed = "\n\n❌ Loss happened"
 
-m_like = "✅ Anda mengerimkan username kepada lawan bicara!\n\n✅ You send the username to the other person"
+m_like = "\n\n✅ You send the username to the other person"
 
-m_dislike_user = "Obrolan Telah Berakhir\n\nChat Has Ended"
+m_dislike_user = "\n\nChat Has Ended"
 
 m_dislike_user_to = (
-    "⛔ Lawan bicara anda meninggalkan obrolan\n\n⛔ Your interlocutor leaves the chat"
+    "\n\n⛔ Your interlocutor leaves the chat"
 )
 
-m_send_some_messages = "Bot tidak bisa meneruskan pesan dari bot\n\nThe bot could not continue the pan from the bot"
+m_send_some_messages = "\n\nThe bot could not continue the pan from the bot"
 
-m_has_not_dialog = "Anda tidak sedang dalam obrolan\n\nYou are not in chat"
+m_has_not_dialog = "\n\nYou are not in chat"
 
 dislike_str = "⛔ Exit"
 
-like_str = "📠 Kirim Kontak"
+like_str = "📠 Send Contact‌‌"
 
 
 def m_all_like(x):
